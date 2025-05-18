9162e1c12aa1557f244faf19e63ac7ccd1621772
Jose Nimo <nimojose@gmail.com>
2025-05-18 15:55:34 +0200

    filtering refactor

---
+++ src/opendvp/filtering/filter_by_thresholds.py
@@ -0,0 +1,15 @@
+
+
+def filter_adata_by_gates(adata: ad.AnnData, gates: pd.DataFrame, sample_id=None) -> ad.AnnData:
+    """ Filter the adata object by the gates """
+    logger.info(" ---- filter_adata_by_gates : version number 1.0.0 ----")
+    time_start = time.time()
+    assert gates.marker_id.isin(adata.var.index).all(), "Some markers in the gates are not present in the adata object"
+    
+    if sample_id is not None:
+        assert sample_id in gates.columns, "The sample_id is not present in the gates"
+        gates = gates[gates['sample_id']==sample_id]
+    
+    adata = adata[:, gates.marker_id]
+    logger.info(f" ---- filter_adata_by_gates is done, took {int(time.time() - time_start)}s  ----")
+    return adata

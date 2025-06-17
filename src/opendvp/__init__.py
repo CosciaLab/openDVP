d06c86e8623d2ade73f3d45c232ab7ae66952e7a
Jose Nimo <nimojose@gmail.com>
2025-06-17 16:55:18 +0200

    last commit on main branch

--- src/opendvp/__init__.py
+++ src/opendvp/__init__.py
@@ -1,5 +1,12 @@
 from . import imaging, io, metrics, plotting, preprocessing, tools, utils
 
+try:
+    from importlib.metadata import version as _version
+except ImportError:
+    from importlib_metadata import version as _version  # type: ignore
+
+__version__ = _version("openDVP")
+
 __all__ = [
     "io",
     "tools",

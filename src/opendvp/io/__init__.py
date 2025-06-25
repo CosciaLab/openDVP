a2d75c079069a1b0ea7f35b98e6639dec4e5aae0
Jose Nimo <nimojose@gmail.com>
2025-06-25 10:55:40 +0200

    removed geojson_to_sdata from __init__.py

--- src/opendvp/io/__init__.py
+++ src/opendvp/io/__init__.py
@@ -3,7 +3,6 @@ from .adata_to_qupath import adata_to_qupath
 from .DIANN_to_adata import DIANN_to_adata
 from .export_adata import export_adata
 from .export_figure import export_figure
-from .geojson_to_sdata import geojson_to_sdata
 from .import_perseus import import_perseus
 from .import_thresholds import import_thresholds
 from .quant_to_adata import quant_to_adata
@@ -17,7 +16,6 @@ __all__ = [
     "import_thresholds",
     "export_adata",
     "export_figure",
-    "geojson_to_sdata",
     "quant_to_adata",
     "segmask_to_qupath",
     "sdata_to_qupath",
--- tests/io/test_import_perseus.py
+++ tests/io/test_import_perseus.py
@@ -1,5 +1,14 @@
 from opendvp.io import import_perseus
 
-def test_import_perseus_callable() -> None:
-    """Test that import_perseus is importable and callable."""
-    assert callable(import_perseus)
+def test_import_perseus() -> None:
+
+    adata = import_perseus(
+        path_to_perseus_txt="../test_data/io/Perseus_v1.6.15.0.txt",
+        n_var_metadata_rows=5
+    )
+    
+    test_shape = (11, 3535)
+    assert adata.shape == test_shape
+    test_list = ['Column Name','Heart_Condition','MI_Region','replicate_number','Group_Type']
+    assert adata.obs.columns.tolist() == test_list
+

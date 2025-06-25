c897ae03f3091258170e435bd3c074844aadce95
Jose Nimo <nimojose@gmail.com>
2025-06-25 11:01:02 +0200

    test_import_perseus.py

--- tests/io/test_import_perseus.py
+++ tests/io/test_import_perseus.py
@@ -7,8 +7,8 @@ def test_import_perseus() -> None:
         n_var_metadata_rows=5
     )
     
-    test_shape = (11, 3535)
+    test_shape = (11, 3526)
     assert adata.shape == test_shape
-    test_list = ['Column Name','Heart_Condition','MI_Region','replicate_number','Group_Type']
+    test_list = ['Column Name','Heart_Condition','Ischemia_region','Sample_type','Replicate']
     assert adata.obs.columns.tolist() == test_list
 
